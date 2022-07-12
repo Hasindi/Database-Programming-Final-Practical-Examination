42bf1ef6b39a9092eb9d93698a92dc01b45ceb76
hasindi <hmudithya@gmail.com>
2022-07-12 11:43:21 +0530

    Create data Add method

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -3,10 +3,15 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import model.Student;
+import util.CrudUtil;
+
+import java.sql.SQLException;
 
 public class StudentFormController {
 
@@ -35,6 +40,27 @@ public class StudentFormController {
     }
 
     public void addOnAction(ActionEvent actionEvent) {
+        Student s = new Student(
+                txtId.getText(),
+                txtName.getText(),
+                txtEmail.getText(),
+                txtContact.getText(),
+                txtAddress.getText(),
+                txtNIC.getText()
+        );
+
+        try{
+            if (CrudUtil.execute("INSERT INTO student VALUES (?,?,?,?,?,?)",
+                    s.getStudent_id(), s.getStudent_name(), s.getEmail(), s.getContact(), s.getAddress(), s.getNIC())){
+                new Alert(Alert.AlertType.CONFIRMATION,"Data Added Succussfully...!").showAndWait();
+            }
+
+
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+
+        }
     }
 
     public void clearOnAction(ActionEvent actionEvent) {
