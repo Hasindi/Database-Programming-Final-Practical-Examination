eea3f8b38376d0ec9be8e6be1ccbcd9a0e3b753f
hasindi <hmudithya@gmail.com>
2022-07-12 12:06:38 +0530

    loard all Data to table

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -2,20 +2,24 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import model.Student;
 import util.CrudUtil;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StudentFormController {
 
-    public TableView tblAllStudents;
+    public TableView<Student> tblAllStudents;
     public TableColumn colId;
     public TableColumn colName;
     public TableColumn colEmail;
@@ -32,6 +36,39 @@ public class StudentFormController {
     public JFXTextField txtContact;
     public JFXTextField txtEmail;
     public JFXTextField txtNIC;
+
+    public void initialize() throws SQLException, ClassNotFoundException {
+        colId.setCellValueFactory(new PropertyValueFactory("student_id"));
+        colName.setCellValueFactory(new PropertyValueFactory("student_name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory("email"));
+        colContact.setCellValueFactory(new PropertyValueFactory("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory("address"));
+        colNIC.setCellValueFactory(new PropertyValueFactory("NIC"));
+
+
+        loadAllStudents();
+
+    }
+
+    private void loadAllStudents() throws SQLException, ClassNotFoundException {
+        ResultSet result = CrudUtil.execute("SELECT * FROM student");
+
+        ObservableList<Student> obList = FXCollections.observableArrayList();
+
+        while (result.next()) {
+            obList.add(
+                    new Student(
+                            result.getString(1),
+                            result.getString(2),
+                            result.getString(3),
+                            result.getString(4),
+                            result.getString(5),
+                            result.getString(6)
+                    )
+            );
+            tblAllStudents.setItems(obList);
+        }
+    }
 
     public void updateOnAction(ActionEvent actionEvent) {
     }
